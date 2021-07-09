3194c36142fa4c456c0fadaa7a50b02a000e8124
pavitra971 <pavitrabarwal@gmail.com>
2021-07-09 14:22:02 +0530

    lol

--- project/settings.py
+++ project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-2yk(=ft!ftc$vzk*8!7@zn3q#zc)&o5-cv!8g%v^@ov=b8@g6-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["thisisthetwelfth.herokuapp.com" , "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
