ed4c3ee674f7ac0bab9db3bd58aa833ca7ba49f0
pavitra971 <pavitrabarwal@gmail.com>
2021-07-10 18:03:00 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -121,11 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"), '/ Var / www / static /',]
-#STATIC_ROOT = BASE_DIR / 'static'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"), '/ Var / www / static /',]
+STATIC_ROOT = BASE_DIR /'static'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
